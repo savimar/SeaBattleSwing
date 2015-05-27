ec079d6b214921ce356811cf8a6fb2c6a4b61dd8
Maria Savina <sam409@yandex.ru>
2015-05-27 20:10:01 +0300

    Create MyJDialog.java

---
+++ MyJDialog.java
@@ -0,0 +1,33 @@
+package ru.knaur.lesson.geekbrainsjava.java1.seabattle;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MyJDialog extends JDialog {
+
+    public MyJDialog(String title, int width, int height) {
+        super.setTitle(title);
+        super.setSize(width, height);
+        super.setLocationRelativeTo(null);
+        super.setVisible(true);
+        super.setLayout(new FlowLayout());
+    }
+
+
+    public MyJDialog(String title, int width, int height, Component comp) {
+        this(title, width, height);
+        super.getContentPane().add(comp);
+
+    }
+
+    public MyJDialog(String title, int width, int height, LayoutManager layout) {
+        super.setTitle(title);
+        super.setSize(width, height);
+        super.setLocationRelativeTo(null);
+//        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setLayout(layout);
+        super.setIconImage(new ImageIcon("icon.png").getImage());
+    }
+
+}
