7f8c91d55ddc1ec8fa65b0c80e13e4c74f76db1a
Maria Savina <sam409@yandex.ru>
2015-05-27 19:58:11 +0300

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,22 @@
+package ru.knaur.lesson.geekbrainsjava.java1.seabattle;
+
+public class Game {
+    public static Cell[][] cellsComputer = new Cell[10][10];
+    public static Cell[][] cellsPlayer = new Cell[10][10];
+    public static int scoreComputer = 0;
+
+
+    public void start() {
+        System.out.println("Начинаем игру");
+
+        Field field = new Field();
+
+        cellsComputer = field.setCells(cellsComputer); //вводим игровое поле компьютера
+        cellsPlayer = field.setCells(cellsPlayer); //вводим поле игрока
+
+        cellsComputer = field.setShips(cellsComputer); //устанавливаем корабли на поле компьютера
+
+    }
+
+
+}
