53dd6619e6212405ad7bbe1641402ad274bbc194
Maria Savina <sam409@yandex.ru>
2015-05-27 20:01:13 +0300

    Create ru.domain.lesson2.ru.knaur.lesson.geekbrainsjava.java1.seabattle.GameController.java

---
+++ ru.domain.lesson2.ru.knaur.lesson.geekbrainsjava.java1.seabattle.GameController.java
@@ -0,0 +1,21 @@
+package lesson2.ru.knaur.lesson.geekbrainsjava.java1.seabattle;
+
+public class GameController {
+    public enum playerShoot {МИМО, ПОПАЛ, НЕТ_ВЫСТРЕЛА} //перечисление выстрелов
+
+
+    //синглетон
+    private static GameController instance;
+
+    private GameController() {
+    }
+
+    public static synchronized GameController getInstance() {
+        if (instance == null) {
+            instance = new GameController();
+        }
+        return instance;
+    }
+
+
+}
