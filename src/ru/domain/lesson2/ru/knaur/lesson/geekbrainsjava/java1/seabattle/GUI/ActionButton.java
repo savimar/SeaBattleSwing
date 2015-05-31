8890f97da97fe58a5f505721c529346ba9779da2
savimar <sam409@yandex.ru>
2015-05-31 22:44:10 +0300

    добавлены звуки

--- src/ru/domain/lesson2/ru/knaur/lesson/geekbrainsjava/java1/seabattle/GUI/ActionButton.java
+++ src/ru/domain/lesson2/ru/knaur/lesson/geekbrainsjava/java1/seabattle/GUI/ActionButton.java
@@ -4,9 +4,12 @@ import ru.domain.lesson2.ru.knaur.lesson.geekbrainsjava.java1.seabattle.Grafics.
 import ru.domain.lesson2.ru.knaur.lesson.geekbrainsjava.java1.seabattle.Logics.Game;
 
 import javax.swing.*;
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class ActionButton implements ActionListener {
     public int x = -1;
@@ -23,65 +26,92 @@ public class ActionButton implements ActionListener {
             if (thread.isAlive()) {
                 thread.interrupt();
             }*/
-        } else if (!isStart) {
+        } else {
+            if (!isStart) {
 
 
-            String str = "";
-            String buttonText = "";
-            Color color = null;
-            SoundPlayer.playSound("44.wav").join();
+                String str = "";
+                String buttonText = "";
+                Color color = null;
+                String path = "";
 
-            try {
-                GameController.setShootPlayer(this.x, this.y, this); //игровой цикл получения координат игрок
-            } catch (Exception e1) {
-                e1.printStackTrace();
+
+                try {
+                    GameController.setShootPlayer(this.x, this.y, this); //игровой цикл получения координат игрок
+                     } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+
+
+                if (getShoot.equals("МИМО")) {
+                    str = "Мимо!";
+                    buttonText = "-";
+                    color = Color.RED;
+                    path = "c://orchhit.wav";
+
+                } else if (getShoot.equals("ПОПАЛ")) {
+                    GameController.getWounded(this.x, this.y, this);
+                    if (this.shipShoot.equals("РАНЕН")) {
+                        str = "Ранен";
+                        path = "c://44.wav";
+                        buttonText = "+";
+                    color = Color.BLUE;
+                    } else {
+                        str = "Убит";
+                        buttonText = "+";
+                        color = Color.BLUE;
+                        path = "c://sdwin.wav";
+                    }
+                }
+
+                if (!GameController.isWinner) {
+
+                    JOptionPane.showMessageDialog(null, str, "выстрел", JOptionPane.PLAIN_MESSAGE);
+                     File wavFile = new File(path);
+                    AudioClip sound = null;
+                    try {
+
+                        sound = Applet.newAudioClip(wavFile.toURL());
+                    } catch (Exception e2) {
+                        e2.printStackTrace();
+                    }
+                    sound.play();
+                }
+
+
+
+                if (!GameController.isWinner) GameWindow.jtxtResult.setText(str);
+                if (e.getSource() instanceof JButton && !GameController.isWinner) {
+                    MyJButton btn = (MyJButton) e.getSource();
+                    Font f = new Font("Courier", Font.BOLD, 18);
+
+                    btn.setFont(f);
+                    btn.setForeground(color);
+                    btn.setText(buttonText);
+
+                     File wavFile = new File(path);
+
+
+                } else if (isStart && (Game.cellsComputer[0][0] == null)) { //если нажата кнопка старта
+                    Game game = new Game();
+                    game.start(); //начинаем игру
+                }
             }
-
-
-            if (getShoot.equals("МИМО")) {
-                str = "Мимо!";
-                buttonText = "-";
-                color = Color.RED;
-            } else if (getShoot.equals("ПОПАЛ")) {
-                GameController.getWounded(this.x, this.y, this);
-                if (this.shipShoot.equals("РАНЕН")) str = "Ранен";
-                else str = "Убит";
-                buttonText = "+";
-                color = Color.BLUE;
-            }
-
-            if (!GameController.isWinner)
-                JOptionPane.showMessageDialog(null, str, "выстрел", JOptionPane.PLAIN_MESSAGE);
-
-
-            if (!GameController.isWinner) GameWindow.jtxtResult.setText(str);
-            if (e.getSource() instanceof JButton && !GameController.isWinner) {
-                MyJButton btn = (MyJButton) e.getSource();
-                Font f = new Font("Courier", Font.BOLD, 18);
-
-                btn.setFont(f);
-                btn.setForeground(color);
-                btn.setText(buttonText);
-            }
-
-        } else if (isStart && (Game.cellsComputer[0][0] == null)) { //если нажата кнопка старта
-            Game game = new Game();
-            game.start(); //начинаем игру
         }
     }
 
 
-    public ActionButton(int x, int y) {
-        this.x = x;
-        this.y = y;
+        public ActionButton( int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public ActionButton() {
+        }
+
+        public ActionButton( boolean isStart){
+            this.isStart = isStart;
+        }
+
+
     }
-
-    public ActionButton() {
-    }
-
-    public ActionButton(boolean isStart) {
-        this.isStart = isStart;
-    }
-
-
-}
